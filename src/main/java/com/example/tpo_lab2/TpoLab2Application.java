c97ff07adef5c6e98833084f18a7a711c5b2c9f1
Oleksandr Iievliev <sXXXX@pjwstk.edu.pl>
2026-03-11 10:34:26 +0100

    "Project creation"

---
+++ src/main/java/com/example/tpo_lab2/TpoLab2Application.java
@@ -0,0 +1,13 @@
+package com.example.tpo_lab2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpoLab2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpoLab2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tpo_lab2/TpoLab2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tpo_lab2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpoLab2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
